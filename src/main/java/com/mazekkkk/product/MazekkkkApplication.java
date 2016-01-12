7ba0f3af1ea645afa3f8e85ca41582416bbeb368
mazekkkk <mazekkkk@hotmail.com>
2016-01-12 18:03:42 +0800

    init project
    初始化项目

---
+++ src/main/java/com/mazekkkk/product/MazekkkkApplication.java
@@ -0,0 +1,12 @@
+package com.mazekkkk.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazekkkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazekkkkApplication.class, args);
+	}
+}
