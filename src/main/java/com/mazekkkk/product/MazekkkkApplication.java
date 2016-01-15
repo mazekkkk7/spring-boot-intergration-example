56011889ae4967521a7ab3dbebc1a13e5c0daed2
mazekkkk <mazekkkk@hotmail.com>
2016-01-15 09:27:42 +0800

    删除多余文件

--- src/main/java/com/mazekkkk/product/MazekkkkApplication.java
+++ src/main/java/com/mazekkkk/product/MazekkkkApplication.java
@@ -2,11 +2,14 @@ package com.mazekkkk.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MazekkkkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MazekkkkApplication.class, args);
 	}
+
 }
--- src/main/java/com/mazekkkk/product/controller/HelloController.java
+++ src/main/java/com/mazekkkk/product/controller/HelloController.java
@@ -1,18 +1,34 @@
 package com.mazekkkk.product.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by mazekkkk on 16/1/12.
  */
 @RestController
+@Component
 public class HelloController {
+
+    public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private  Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
     public String index() {
         return "build Spring boot Success!";
     }
 
+    @Scheduled(fixedRate = 5000)
+    public void taskScheduled(){
+        logger.info(sdf.format(new Date()));
+    }
 
 }
