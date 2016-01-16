a0daaa03c21637edb827a14ab5628a0aec97343c
mazekkkk <mazekkkk@hotmail.com>
2016-01-16 12:10:03 +0800

    定时任务按启动时间执行（仅执行一次）

--- src/main/java/com/mazekkkk/product/schedule/ScheduleTask.java
+++ src/main/java/com/mazekkkk/product/schedule/ScheduleTask.java
@@ -3,6 +3,7 @@ package com.mazekkkk.product.schedule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -15,7 +16,7 @@ import java.util.Date;
 @Component
 public class ScheduleTask {
 
-    private TaskSchedulerImpl taskScheduler = new TaskSchedulerImpl();
+    private ConcurrentTaskScheduler concurrentTaskScheduler = new ConcurrentTaskScheduler();
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -24,10 +25,10 @@ public class ScheduleTask {
     private Calendar calendar = Calendar.getInstance();
 
     public void schedulerGenerate(){
-        calendar.set(2016,0,16,11,53,55);
+        calendar.set(2016,0,16,12,8,55);
         logger.info(sdf.format(calendar.getTime())+"----------------------------------------");
         SchedulerRunnable task = new SchedulerRunnable();
-        taskScheduler.schedule(task,calendar.getTime());
+        concurrentTaskScheduler.schedule(task,calendar.getTime());
     }
 
     @Scheduled(fixedRate = 5000)
