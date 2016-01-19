a2fdf0dcb44542bd0a89b35f47bad5435c98c587
mazekkkk <mazekkkk@hotmail.com>
2016-01-19 18:28:15 +0800

    分模块项目

--- product-controller/src/main/java/cn/mazekkkk/product/controller/MazekkkkApplication.java
+++ product-controller/src/main/java/cn/mazekkkk/product/controller/MazekkkkApplication.java
@@ -1,5 +1,6 @@
 package cn.mazekkkk.product.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfigura
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -15,12 +17,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication(exclude = DispatcherServletAutoConfiguration.class)
 @EnableScheduling
 @EnableWebMvc
+@Configuration
 @EnableAutoConfiguration
+//@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(value = "cn.mazekkkk.product")
 public class MazekkkkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MazekkkkApplication.class, args);
+		SpringApplication app = new SpringApplication(MazekkkkApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 
 	@Bean(name = "root")
