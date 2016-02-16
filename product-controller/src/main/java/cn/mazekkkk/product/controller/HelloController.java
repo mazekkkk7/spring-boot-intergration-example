d443283a2dd2de88ba3576be706e0c8f3088a5ab
mazekkkk <mazekkkk@hotmail.com>
2016-02-16 17:13:26 +0800

    提交分页

--- product-controller/src/main/java/cn/mazekkkk/product/controller/HelloController.java
+++ product-controller/src/main/java/cn/mazekkkk/product/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -64,6 +65,12 @@ public class HelloController {
     public Object getAllAddress(){
         logger.info("selectAllAddress-----------------------");
         return xncAddressService.getAllAddress();
+    }
+
+    @RequestMapping("/greetingView")
+    public String greeting(@RequestParam(value = "name", required = false,defaultValue = "world") String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
     }
 
 
